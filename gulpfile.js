ff469fcb5970bfc2e8ecb0fd5c427f970b8fcfdc
jartek <jartek.91@gmail.com>
2015-01-25 23:09:51 +0530

    Grid basics

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,34 @@ var rename = require('gulp-rename');
 var prefixer = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
 
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('browser-sync', function() {
+  browserSync(['index.html'], {
+    port: 8080,
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
 gulp.task('sass', function() {
   return gulp.src('./src/*.scss')
     .pipe(prefixer('last 10 versions'))
     .pipe(rename('bedrock.min.css'))
-    .pipe(sass())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
     .pipe(minifycss())
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./'))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('src/*.scss', ['sass']);
+  gulp.watch('src/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'watch', 'browser-sync']);
